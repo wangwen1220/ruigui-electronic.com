8301c472c58aeaed779e7b435315f4f4dd76aee0
wangwen1220 <wangwen1220@139.com>
2015-02-10 22:50:18 +0800

    'home'

--- wp-content/themes/ruigui/page-about-us.php
+++ wp-content/themes/ruigui/page-about-us.php
@@ -9,42 +9,18 @@
 
 get_header(); ?>
 
-  <?php the_title(); ?>
-  <?php the_category(', ') ?>
-  <?php query_posts('category_name=showcase&showposts=4'); ?>
+  <?php query_posts('category_name=about-us&showposts=5'); ?>
   <?php if (have_posts()) : ?>
     <?php while (have_posts()) : the_post(); ?>
-
+      <?php echo the_content(); ?>
     <?php endwhile; ?>
-  <?php else: ?>
-    <?php get_template_part( 'no-results', 'archive' ); ?>
+  <?php else : ?>
+    <?php echo "No Content."; ?>
   <?php endif; ?>
+  <?php wp_reset_query(); ?>
 
-  xx<?php
-  global $post;
-  if($post->post_parent){
-      $children = wp_list_pages("title_li=&child_of=".$post->post_parent."&echo=0");
-  } else {
-    echo 'parent';
-      $children = wp_list_pages("title_li=&child_of=".$post->ID."&echo=0");
-  }
-  if ($children) {
-      echo '<ul>';
-          echo $children;
-      echo '</ul>';
-  }
-  ?>
-<br>
-  <?php
-  $output = 'xxx';
-  $page = $post->ID;
-  if ($post->post_parent) {
-    $page = $post->post_parent;
-  }
-  $children=wp_list_pages( 'echo=0&child_of=' . $page . '&title_li=' );
-  if ($children) {
-    $output = wp_list_pages ('echo=0&child_of=' . $page . '&title_li=<h2>Child Pages</h2>');
-  }
-  echo $children;
-  ?>
+  <div class="menu">
+    <?php wp_list_categories(); ?>
+  </div>
+
 <?php get_footer(); ?>
--- wp-content/themes/ruigui/page.php
+++ wp-content/themes/ruigui/page.php
@@ -11,39 +11,21 @@ get_header(); ?>
 
 	<?php while ( have_posts() ) : the_post(); ?>
 
-		<?php get_template_part( 'content', 'page' ); ?>
+		<?php //get_template_part( 'content', 'page' ); ?>
+		<?php the_content(); ?>
 
 	<?php endwhile; ?>
 
-	xx<?php
-	global $post;
-	if($post->post_parent){
+	<?php
+	  if ($post->post_parent) :
 	    $children = wp_list_pages("title_li=&child_of=".$post->post_parent."&echo=0");
-	} else {
-		echo 'parent';
+	  else :
 	    $children = wp_list_pages("title_li=&child_of=".$post->ID."&echo=0");
-	}
-	if ($children) {
-	    echo '<ul>';
-	        echo $children;
-	    echo '</ul>';
-	}
-	?>
+	  endif;
 
-	ww<?php
-	$output = wp_list_pages('echo=0&depth=1&title_li=<h2>Top Level Pages </h2>' );
-	if (is_page()) {
-		echo "sssssss";
-	  $page = $post->ID;
-	  if ($post->post_parent) {
-	    $page = $post->post_parent;
-	  }
-	  $children=wp_list_pages( 'echo=0&child_of=' . $page . '&title_li=' );
-	  if ($children) {
-	    $output = wp_list_pages ('echo=0&child_of=' . $page . '&title_li=<h2>Child Pages</h2>');
-	  }
-	}
-	echo $output;
+	  if ($children) :
+	    echo '<ul>'.$children.'</ul>';
+	  endif;
 	?>
 
 <?php get_footer(); ?>
